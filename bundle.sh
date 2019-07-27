58aeb3ab46df20e86fe89aeb38a23969cac9a3ad
Criveti Mihai <crivetimihai@gmail.com>
2019-07-27 14:01:43 -0500

    Workstation tools and dotfiles

---
+++ bundle.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# exit when any command fails
+set -e
+
+# Run lint
+yamllint roles/*
+ansible-lint roles/*
+
+# Release
+my_release=$(mazer build | awk 'END{print $NF}')
+
+
+echo "Release ${my_release} to ansible-galaxy?"
+select yn in "Yes" "No"; do
+    case $yn in
+        Yes ) mazer publish ${my_release}; break;;
+        No ) exit;;
+    esac
+done
